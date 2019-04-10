d637506e3d24cd7e255cb3684e75d1018a37f40d
Administrator <Administrator@pc-16>
2019-04-10 15:05:39 +0900

    this commit is the first time commit,
    GitDemo.java file is uploaded.

---
+++ GithubTest/src/org/study/demo/GitDemo.java
@@ -0,0 +1,13 @@
+package org.study.demo;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Github!");
+		//this file to be uploaded to GitHub.
+		
+		
+
+	}
+
+}
